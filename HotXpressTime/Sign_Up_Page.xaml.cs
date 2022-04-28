d7d5e0a6000d0a89d66383f1e4f11c2beff3c421
RiggityRussell <arlt9@mail.nmc.edu>
2022-04-28 19:26:32 -0400

    Passwords must be the same between boxs

--- HotXpressTime/Sign_Up_Page.xaml.cs
+++ HotXpressTime/Sign_Up_Page.xaml.cs
@@ -18,20 +18,46 @@ namespace HotXpressTime
     /// <summary>
     /// Interaction logic for Sign_Up_Page.xaml
     /// </summary>
+    /// 
+    
+
     public partial class Sign_Up_Page : Page
     {
+        
+
         public Sign_Up_Page()
         {
+            
             //test
             InitializeComponent();
+             
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (passwordBox.Password == "Password")
+            {
+                statusText.Text = "'Password' is not allowed as a password.";
+            }
+            else
+            {
+                statusText.Text = string.Empty;
+            }
         }
 
         private void CreateProfileButton_Click(object sender, RoutedEventArgs e)
         {
 
             string username = UsernameBox.Text.ToString();
-            string password = PasswordBox.Text.ToString();
-            string reEnterPass = PasswordBox_ReEnter.Text.ToString();
+            string password = passwordBox.Password.ToString();
+            string reEnterPass = passwordBox_ReEnter.Password.ToString();
+
+            if (password != reEnterPass)
+            {
+                passwordBox.Clear();
+                passwordBox_ReEnter.Clear();
+                MessageBox.Show("Passwords must be the same.", "Re-Enter Passwords", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
 
             bool valid = Utilities.IsCredentialsValid(username, password, reEnterPass);
 
@@ -44,7 +70,7 @@ namespace HotXpressTime
                 //Create a textbox to display a generic error for now
                 //"Username or password was missing, or passwords did not match. Try again"
                 //If we have time give a message based on what was actually missing
-            }
+             }
 
             //Some sort of regristaion completed toast?
             //"Profile successfully completed!"
