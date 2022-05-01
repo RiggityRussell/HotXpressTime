61573ce5f9652330023ed75a68e2e35d4ef7163a
Mel <emanm@mail.nmc.edu>
2022-04-30 20:02:41 -0400

    Item Shopping Cart Window

---
+++ HotXpressTime/Bacon_Figs.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotXpressTime
+{
+    /// <summary>
+    /// Interaction logic for Bacon_Figs.xaml
+    /// </summary>
+    public partial class Bacon_Figs : Page
+    {
+        public Bacon_Figs()
+        {
+            InitializeComponent();
+        }
+    }
+}
