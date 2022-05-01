aa4c06a767b0b1c01f06e64705e1c5def57b0060
Mel <emanm@mail.nmc.edu>
2022-05-01 19:40:25 -0400

    Shopping Cart

---
+++ HotXpressTime/ShoppingCart.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotXpressTime
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class ShoppingCart : Page
+    {
+        public ShoppingCart()
+        {
+            InitializeComponent();
+        }
+
+        private void ContinueShopping_Click(object sender, RoutedEventArgs e)
+        {
+            ContinueShopping.Content = new MainWindow();
+        }
+
+        private void Checkout_Click(object sender, RoutedEventArgs e)
+        {
+            Checkout.Content = new MainWindow();
+        }
+
+        private void Total_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Your Total is: ");
+        }
+    }
+}
