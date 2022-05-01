d871497eaad7ae3c7746bc67a17d3f88f23a8d4e
Skitten168 <kittens@mail.nmc.edu>
2022-05-01 10:59:16 -0400

    commit

--- HotXpressTime/MenuItems/FigSmoothie.xaml.cs
+++ HotXpressTime/MenuItems/FigSmoothie.xaml.cs
@@ -24,5 +24,10 @@ namespace HotXpressTime.MenuItems
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
